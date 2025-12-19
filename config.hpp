6fcdb4401c9dfceba619fd87eba50a72c90cb9c4
Srinivas0694 <srinivasudiga73@gmail.com>
2025-12-19 21:16:52 +0530

    Create config.hpp

---
+++ config.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#define WIFI_SSID       "Awfis-Temp"
+#define WIFI_PASSWORD   "Awfis#2025#"
+
+#define AWS_ENDPOINT    "arpig3ykn8d83-ats.iot.us-east-1.amazonaws.com"
+#define AWS_PORT        8883
+#define MQTT_TOPIC      "esp32/airquality"
+#define DEVICE_ID       "esp32_01"
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+#endif
