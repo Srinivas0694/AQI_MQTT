26c11faac2852d478640735887061248339de155
Srinivas0694 <srinivasudiga73@gmail.com>
2025-12-24 10:50:32 +0530

    Update sensors.cpp

--- sensors.cpp
+++ sensors.cpp
@@ -1,46 +1,66 @@
 #include "sensors.hpp"
+#include "config.hpp"
+
 #include <Wire.h>
 #include <SensirionI2CSen5x.h>
+#include <SensirionI2CScd4x.h>
 #include <Adafruit_BME680.h>
 
 SensirionI2CSen5x sen5x;
+SensirionI2cScd4x scd4x;      // ✔ correct class name
 Adafruit_BME680 bme;
 
 void sensors_init() {
-  Wire.begin(21, 22);
+  Wire.begin(SDA_PIN, SCL_PIN);
 
+  // ---------- SEN55 ----------
   sen5x.begin(Wire);
   sen5x.startMeasurement();
 
+  // ---------- BME688 ----------
   if (!bme.begin(0x76)) {
     Serial.println("❌ BME688 not found");
   }
+
+  // ---------- SCD40 ----------
+  scd4x.begin(Wire, 0x62);     // ✔ correct signature
+  scd4x.stopPeriodicMeasurement();
+  delay(1000);
+  scd4x.startPeriodicMeasurement();
+
+  Serial.println("✅ SCD40 started");
 }
 
 SensorData sensors_read() {
+  SensorData data = {};
 
-    SensorData data;   // MUST exist
+  // ---------- SEN55 ----------
+  sen5x.readMeasuredValues(
+    data.pm1_0,
+    data.pm2_5,
+    data.pm4_0,
+    data.pm10,
+    data.humidity,
+    data.temperature,
+    data.voc,
+    data.nox
+  );
 
-    uint16_t error = sen5x.readMeasuredValues(
-        data.pm1_0,
-        data.pm2_5,
-        data.pm4_0,
-        data.pm10,
-        data.humidity,
-        data.temperature,
-        data.voc,   // float
-        data.nox    // float
-    );
+  // ---------- SCD40 ----------
+  bool dataReady = false;
+  scd4x.getDataReadyStatus(dataReady);   // correct API
 
-    if (error) {
-        Serial.print("SEN55 read error: ");
-        Serial.println(error);
+  if (dataReady) {
+    uint16_t co2;
+    float temp, hum;
+
+    if (scd4x.readMeasurement(co2, temp, hum) == 0) {
+      if (co2 > 0) {
+        data.co2 = co2;
+      }
     }
+  }
 
-    return data;
+  return data;
 }
-
-
-
-
 
