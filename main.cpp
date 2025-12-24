32f770126e22387b4d98d43d7887c3d82f63cc71
Srinivas0694 <srinivasudiga73@gmail.com>
2025-12-24 10:50:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,33 +10,29 @@ void setup() {
   Serial.println("ESP32 Air Quality Project Started");
 
   sensors_init();
-  Serial.println("Sensors initialized");
-
   display_init();
-  Serial.println("OLED initialized");
-
   aws_init();
-  Serial.println("AWS IoT connected");
 }
 
 void loop() {
   SensorData data = sensors_read();
 
- Serial.println("---- OLED DATA ----");
- Serial.print("PM1.0 : "); Serial.println(data.pm1_0);
- Serial.print("PM2.5 : "); Serial.println(data.pm2_5);
- Serial.print("PM4.0 : "); Serial.println(data.pm4_0);
- Serial.print("PM10  : "); Serial.println(data.pm10);
- Serial.print("Temp  : "); Serial.println(data.temperature);
- Serial.print("Hum   : "); Serial.println(data.humidity);
- Serial.print("VOC   : "); Serial.println(data.voc);
- Serial.print("NOx   : "); Serial.println(data.nox);
-
+  // ✅ SERIAL MONITOR OUTPUT (ALL PARAMETERS)
+  Serial.println("---- SENSOR DATA ----");
+  Serial.print("PM1.0 : "); Serial.println(data.pm1_0);
+  Serial.print("PM2.5 : "); Serial.println(data.pm2_5);
+  Serial.print("PM4.0 : "); Serial.println(data.pm4_0);
+  Serial.print("PM10  : "); Serial.println(data.pm10);
+  Serial.print("Temp  : "); Serial.println(data.temperature);
+  Serial.print("Hum   : "); Serial.println(data.humidity);
+  Serial.print("CO2   : "); Serial.println(data.co2);
+  Serial.print("VOC   : "); Serial.println(data.voc);
+  Serial.print("NOx   : "); Serial.println(data.nox);
+  Serial.println("---------------------\n");
 
   display_show(data);
   aws_publish(data);
   aws_loop();
 
-  delay(5000);
+  delay(5000);   // 🔴 REQUIRED for SCD40
 }
-
