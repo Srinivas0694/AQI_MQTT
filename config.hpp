5ef5185bb875c32639c649f0c014514094bb8a15
Srinivas0694 <srinivasudiga73@gmail.com>
2025-12-24 10:48:16 +0530

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -3,12 +3,10 @@
 
 #define WIFI_SSID       "Awfis-Temp"
 #define WIFI_PASSWORD   "Awfis#2025#"
-
-#define AWS_ENDPOINT    "arpig3ykn8d83-ats.iot.us-east-1.amazonaws.com"
+#define DEVICE_TOKEN    "esp32_token_01" 
+#define AWS_ENDPOINT    "au7tuevsm9a5y-ats.iot.ap-south-1.amazonaws.com"
 #define AWS_PORT        8883
 #define MQTT_TOPIC      "esp32/airquality"
-#define DEVICE_ID       "esp32_01"
-
 #define SDA_PIN 21
 #define SCL_PIN 22
 
